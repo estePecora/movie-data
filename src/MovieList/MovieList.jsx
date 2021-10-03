557bafb3dafcf7e972441674d8d47de54e349568
estePecora <b.estefaniapecora@gmail.com>
2021-10-02 21:51:28 -0300

    lastcommit

--- src/MovieList/MovieList.jsx
+++ src/MovieList/MovieList.jsx
@@ -1,18 +1,60 @@
 import React, { useState } from 'react'
 import MovieCard from '../MovieCard/MovieCard'
 import FavoriteButton from '../FavoriteButton/FavoriteButton'
+import style from './MovieList.module.css'
 
-function MovieList({moviesToShow, addFavorite})  {
+function MovieList({moviesToShow, movieResult, addFavorite})  {
 
     return (
-        <div>
-            <h1>Movies</h1>
+        <div className={style.moviesMainContainer}>
+
+                <div className={style.listTitleContainer}>
+                    <h1>Movies</h1>
+                </div>
+
+        {movieResult.length != 0 ? 
+
+            <div className={style.movieListContainer}>
+            {movieResult && movieResult.map(el => {
+                return <div>
+                <MovieCard 
+                    addFavorite={addFavorite} 
+                    key={el.title} 
+                    image={el.info.image_url} 
+                    title={el.title} year={el.year} 
+                    genre={el.info.genres}/>
+                <FavoriteButton 
+                    addFavorite={addFavorite} 
+                    fav={el}/>
+                </div>
+                }
+            )}
+            </div>
+
+            :
+
+            <div className={style.movieListContainer}>
             {moviesToShow && moviesToShow.slice(0, 3).map(el => {
                 return <div>
-                <MovieCard addFavorite={addFavorite} key={el.title} image={el.info.image_url} title={el.title} year={el.year} genre={el.info.genres}/>
-                <FavoriteButton addFavorite={addFavorite} fav={el}/>
-                </div>}
-                )}
+                <MovieCard 
+                    addFavorite={addFavorite} 
+                    key={el.title} 
+                    image={el.info.image_url} 
+                    title={el.title} year={el.year} 
+                    genre={el.info.genres}/>
+                <FavoriteButton 
+                    addFavorite={addFavorite} 
+                    fav={el}/>
+                </div>
+                }
+            )}
+            </div>
+
+    
+    
+    
+    }
+                
             
         </div>
     )
--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import style from './SearchBar.module.css'
 
-function SearchBar({onSearch}) {
+function SearchBar({onSearch, onGoBack}) {
     const [movie, setMovie] = useState("")
 
     return (
-        <div>
+        <div className={style.mainSearchContainer}>
             <form onSubmit={(event) => {
                 event.preventDefault();
                 onSearch(movie);
@@ -15,13 +16,21 @@ function SearchBar({onSearch}) {
                 placeholder="Find a movie..."
                 value={movie}
                 onChange={event => setMovie(event.target.value)}
-                
+                className={style.searchBarstyle}
             />
             <input 
-            type="submit" value="Search"
+                type="submit" value="Search"
+                className={style.searchButton}
             
             />
             </form>
+
+            <button onClick={(event) => {
+                event.preventDefault();
+                onGoBack()
+                }}
+                className={style.backButton}
+            >Back to all movies</button>
             
         </div>
     )
